28ee4b20bd75ac4c14b7653da9443133ea2be99a
marcosfellipec <marcosfellipec@gmail.com>
2017-05-22 17:22:59 -0300

    Criando modal

---
+++ src/main/java/br/com/imovelcontrol/service/CadastroLocatarioService.java
@@ -0,0 +1,30 @@
+package br.com.imovelcontrol.service;
+
+import br.com.imovelcontrol.model.Locatario;
+import br.com.imovelcontrol.repository.Locatarios;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by marcosfellipec on 18/05/17.
+ */
+
+@Service
+public class CadastroLocatarioService {
+
+
+    @Autowired
+    private Locatarios locatarios;
+
+    @Transactional
+    public void salvar(Locatario locatario){
+        locatarios.save(locatario);
+    }
+
+    @Transactional
+    public void excluir(Locatario locatario){
+        locatarios.delete(locatario);
+    }
+}
