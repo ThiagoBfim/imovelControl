c4718bd0a5a0209a8004e5add371aa16f0b303b5
Thiago <thiagobomfim@gmail.com>
2017-05-26 17:08:21 -0300

    Criacao do atributo login

--- src/main/java/br/com/imovelcontrol/controller/RelatorioController.java
+++ src/main/java/br/com/imovelcontrol/controller/RelatorioController.java
@@ -10,12 +10,8 @@ import java.util.Map;
 import br.com.imovelcontrol.dto.PeriodoRelatorioDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import sun.plugin.javascript.navig4.Link;
 
 @Controller
 @RequestMapping("/relatorios")
