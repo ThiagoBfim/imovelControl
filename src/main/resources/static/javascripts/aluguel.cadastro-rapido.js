f767a8dc7d19b2b982ddb07381b308aca3c8b040
ThiagoBfim <thiagobomfim1995@gmail.com>
2017-06-24 17:57:31 -0300

    Criacao da modal para o locatario

--- src/main/resources/static/javascripts/aluguel.cadastro-rapido.js
+++ src/main/resources/static/javascripts/aluguel.cadastro-rapido.js
@@ -1,70 +1,63 @@
 var Brewer = Brewer || {};
 
-Brewer.AluguelCadastroRapido = (function() {
+Brewer.AluguelCadastroRapido = (function () {
 
-	function AluguelCadastroRapido() {
-		this.modal = $('#modalCadastroRapidoAluguel');
-		this.botaoSalvar = this.modal.find('.js-modal-cadastro-salvar-button');
-		this.containerMensagemErro = $('.js-messagem-cadastro-rapido');
-		// this.inputComplemento = $('#complemento');
-		this.inputAluguel = $('#aluguel');
-		this.form = this.modal.find('form');
-		this.url = this.form.attr('action');
-	}
+    var modal = $('#modalCadastroLocatario');
 
-	AluguelCadastroRapido.prototype.iniciar = function() {
-		this.form.on('submit', function(event) {
-			event.preventDefault();
-		});
+    function AluguelCadastroRapido() {
+        this.botaoSalvar = $('.js-modal-locatario');
+        // this.inputComplemento = $('#complemento');
+        this.codigoAluguel = this.botaoSalvar.data('codigo');
+        this.locatarioAluguel = $('#codigoAluguel');
 
-		this.modal.on('hide.bs.modal', onModalClose.bind(this));
-		this.botaoSalvar.on('click', onBotaoSalvarClick.bind(this));
-	}
 
-	function onModalClose() {
-		this.containerMensagemErro.addClass('hidden');
-		this.form.find('.form-group').removeClass('has-error');
-	}
+        var token = $("input[name='_crsf']").val();
+        var header = "X-CSRF-TOKEN";
+        $(document).ajaxSend(function (e, xhr, options) {
+            xhr.setRequestHeader(header, token);
+        });
+    }
 
-	function onBotaoSalvarClick() {
-		var aluguel = this.inputAluguel.val();
-		$.ajax({
-			url : this.url,
-			method : 'POST',
-			contentType : 'application/json',
-			data : {
-				aluguel : aluguel
-			},
-			error : onErroSalvandoaluguel.bind(this),
-			success : onAluguelSalvo.bind(this)
-		});
-	}
+    AluguelCadastroRapido.prototype.iniciar = function () {
+        this.botaoSalvar.on('click', onBotaoSalvarClick.bind(this));
+    }
 
-	function onErroSalvandoaluguel(obj) {
-		console.log("erro");
-		var msgError = obj.responseText;
-		this.containerMensagemErro.removeClass('hidden');
-		this.containerMensagemErro.html('<span>' + msgError + '</span>');
-		this.form.find('.form-group').addClass('has-error');
-	}
+    function onBotaoSalvarClick() {
+        console.log(this.codigoAluguel);
+        $.getJSON('http://localhost:8080/ImovelControl/locatario/' + this.codigoAluguel, function (data) {
+            meu_callback(data);
+        });
 
-	function onAluguelSalvo(aluguel) {
-		console.log("salvo");
-		var comboAluguel = $('#aluguel');
-		comboAluguel.append('<option value=' + aluguel.codigo + '>'
-				+ aluguel.complemento + '</option>');
 
-		comboAluguel.val(aluguel.codigo);
-		this.modal.modal('hide');
+        this.locatarioAluguel.val(this.codigoAluguel);
+    }
 
-	}
+    function meu_callback(conteudo) {
 
-	return AluguelCadastroRapido;
+        var inputNome = $('#nomeLocatario');
+        var inputCpf = $('#cpf');
+        var inputTelefone = $('#telefone');
+        var inputCodigo = $('#codigoLocatario');
+
+        if (!("erro" in conteudo)) {
+            //Atualiza os campos com os valores.
+            inputCodigo.val(conteudo.codigo);
+            inputNome.val(conteudo.nome);
+            inputCpf.val(conteudo.cpf);
+            inputTelefone.val(conteudo.telefone);
+            modal.modal();
+        } //end if.
+        else {
+            console.log('erro');
+        }
+    }
+
+    return AluguelCadastroRapido;
 
 }());
 
-$(function() {
-	var aluguelCadastroRapido = new Brewer.AluguelCadastroRapido();
-	aluguelCadastroRapido.iniciar();
+$(function () {
+    var aluguelCadastroRapido = new Brewer.AluguelCadastroRapido();
+    aluguelCadastroRapido.iniciar();
 
 })
