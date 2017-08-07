34703ea151e4e6d77ff3560c8b9859c622650dd7
marcosfellipec <marcosfellipec@gmail.com>
2017-08-07 09:45:47 -0300

    impeditivo de cadastro de locatário inválido corrigido

--- src/main/java/br/com/imovelcontrol/service/CadastroLocatarioService.java
+++ src/main/java/br/com/imovelcontrol/service/CadastroLocatarioService.java
@@ -7,7 +7,9 @@ import br.com.imovelcontrol.controller.converter.FormatUtil;
 import br.com.imovelcontrol.model.Locatario;
 import br.com.imovelcontrol.model.tipoimovel.Aluguel;
 import br.com.imovelcontrol.repository.Locatarios;
+import br.com.imovelcontrol.service.exception.CpfLocatarioInvalidoException;
 import br.com.imovelcontrol.service.exception.CpfLocatarioJaCadastradoException;
+import br.com.imovelcontrol.service.exception.NomeLocatarioInvalidoException;
 import br.com.imovelcontrol.service.exception.TelefoneLocatarioJaCadastradoException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,6 +32,13 @@ public class CadastroLocatarioService {
             throw new CpfLocatarioJaCadastradoException("Já existe um Locatário cadastrado com esse CPF");
         }else if (locatarios.findByTelefone(FormatUtil.removerMascara(locatario.getTelefone())).isPresent()){
             throw  new TelefoneLocatarioJaCadastradoException("Já existe um locatário cadastrado com esse telefone");
+        } else if( FormatUtil.removerMascara(locatario.getCpf()).length() < 11){
+            throw new CpfLocatarioInvalidoException("Cpf Inválido!");
+
+        }else if(FormatUtil.removerMascara(locatario.getTelefone()).length() < 10 ){
+            throw new TelefoneLocatarioJaCadastradoException("Telefone Inválido!");
+        }else if(locatario.getNome().length() < 1){
+            throw new NomeLocatarioInvalidoException("Nome inválido");
         }
         return locatarios.save(locatario);
     }
