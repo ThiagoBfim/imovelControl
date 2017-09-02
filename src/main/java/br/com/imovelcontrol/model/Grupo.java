b2fbb968e307b292d3b788590b034307a2a24a3e
ThiagoBfim <thiagobomfim1995@gmail.com>
2017-09-02 00:46:50 -0300

    Implementação da segurança do acesso ao editar e alterar senha

--- src/main/java/br/com/imovelcontrol/model/Grupo.java
+++ src/main/java/br/com/imovelcontrol/model/Grupo.java
@@ -12,59 +12,70 @@ import org.hibernate.validator.constraints.NotBlank;
 @Entity
 @Table(name = "grupo")
 public class Grupo extends BaseEntity {
-	public static final Long PROPRIETARIO = 2L;
-	public static final Long ADMIN = 1L;
-	@NotBlank(message = "Nome Obrigatório")
-	private String nome;
 
-	@ManyToMany
-	@JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "codigo_grupo"), 
-	inverseJoinColumns = @JoinColumn(name = "codigo_permissao"))
-	private List<Permissao> permissoes;
+    public static final Long ADMIN = 0L;
+    public static final Long PROPRIETARIO = 1L;
+    @NotBlank(message = "Nome Obrigatório")
+    private String nome;
 
-	public String getNome() {
-		return nome;
-	}
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "codigo_grupo"),
+            inverseJoinColumns = @JoinColumn(name = "codigo_permissao"))
+    private List<Permissao> permissoes;
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public Grupo() {
+        /*Construtor Vazio*/
+    }
 
-	public List<Permissao> getPermissoes() {
-		return permissoes;
-	}
+    /**
+     * Construtor para criar o elemento apenas com o codigo.
+     *
+     * @param codigo codigo do grupo.
+     */
+    public Grupo(Long codigo) {
+        this.codigo = codigo;
+    }
 
-	public void setPermissoes(List<Permissao> permissoes) {
-		this.permissoes = permissoes;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
-		return result;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Grupo other = (Grupo) obj;
-		if (codigo == null) {
-			if (other.codigo != null)
-				return false;
-		} else if (!codigo.equals(other.codigo))
-			return false;
-		return true;
-	}
-	
-	
-	
-	
+    public List<Permissao> getPermissoes() {
+        return permissoes;
+    }
+
+    public void setPermissoes(List<Permissao> permissoes) {
+        this.permissoes = permissoes;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Grupo other = (Grupo) obj;
+        if (codigo == null) {
+            if (other.codigo != null)
+                return false;
+        } else if (!codigo.equals(other.codigo))
+            return false;
+        return true;
+    }
+
 
 }
