3768eb80af0222ad504929fa7a8398c4453844f2
Thiago <thiagobomfim@gmail.com>
2017-09-29 23:38:47 -0300

    Refatoração do codigo de configuração do sistema.

--- src/main/java/br/com/imovelcontrol/webservice/ClienteViaCepWS.java
+++ src/main/java/br/com/imovelcontrol/webservice/ClienteViaCepWS.java
@@ -1,10 +1,15 @@
 package br.com.imovelcontrol.webservice;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Thiago on 11/05/2017.
@@ -33,4 +38,20 @@ public class ClienteViaCepWS {
         return json;
     }
 
+    /*Logica para testar o WBService de Cep.*/
+    public static void main(String[] args) throws IOException {
+        String json = buscarCep("69046000");
+        System.out.println(json);
+
+        Map<String, String> mapa = new HashMap<>();
+
+        Matcher matcher = Pattern.compile("\"\\D.*?\": \".*?\"").matcher(json);
+        while (matcher.find()) {
+            String[] group = matcher.group().split(":");
+            mapa.put(group[0].replaceAll("\"", "").trim(), group[1]
+                    .replaceAll("\"", "").trim());
+        }
+
+        System.out.println(mapa);
+    }
 }
