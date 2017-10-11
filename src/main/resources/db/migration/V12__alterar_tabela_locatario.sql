c8081131a102a3920ee1e3523a56fb57f1f221e1
ThiagoBfim <thiagobomfim1995@gmail.com>
2017-10-11 20:22:53 -0300

    Criacao profile para producao

--- src/main/resources/db/migration/V12__alterar_tabela_locatario.sql
+++ src/main/resources/db/migration/V12__alterar_tabela_locatario.sql
@@ -1,6 +1,5 @@
-ALTER TABLE locatario
-ADD codigo_aluguel BIGINT(20) NOT NULL DEFAULT 1;
+ALTER TABLE locatario ADD codigo_aluguel BIGINT(20) NOT NULL DEFAULT 1;
 
-ALTER TABLE locatario
-ADD CONSTRAINT fk_alguel_locatario   FOREIGN KEY (codigo_aluguel) REFERENCES aluguel(codigo);
+ALTER TABLE locatario ADD CONSTRAINT fk_aluguel_locatario  FOREIGN KEY (codigo_aluguel) REFERENCES ALUGUEL(codigo);
+
 
--- src/main/resources/db/migration/V13__alterar_tabela_informacoes_pagamento.sql
+++ src/main/resources/db/migration/V13__alterar_tabela_informacoes_pagamento.sql
@@ -1,6 +1,6 @@
-ALTER TABLE informacao_pagamento
+ALTER TABLE INFORMACAO_PAGAMENTO
 ADD codigo_aluguel BIGINT(20) NOT NULL DEFAULT 1;
 
-ALTER TABLE informacao_pagamento
-ADD CONSTRAINT fk_aluguel_informacao_pagamento   FOREIGN KEY (codigo_aluguel) REFERENCES aluguel(codigo);
+ALTER TABLE INFORMACAO_PAGAMENTO
+ADD CONSTRAINT fk_aluguel_informacao_pagamento   FOREIGN KEY (codigo_aluguel) REFERENCES ALUGUEL(codigo);
 
